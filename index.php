c0bf0503f5c408d71331aa30e0335097f8d48c16
Simone Lee <simonelee@live.dk>
2016-09-19 14:38:12 +0200

    Connection
    Fixing connection

--- index.php
+++ index.php
@@ -2,23 +2,20 @@
 require_once("db_const.php");  
 $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME); 
 $connection->set_charset("utf8");  
-?> 
+?>
 
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Chuck Norris Facts</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
 
-<!doctype html> 
-<html> 
-<head> 
-<meta charset="utf-8"> 
-
-<title>Chuck Norris Facts</title> 
-<link rel="stylesheet" type="text/css" href="css/styles.css">	 
-</head>  
-
-<body> 
-	<header>
-    	<h1>Chuck Norris Facts</h1>
-
-<?php 
+<body>
+<header>
+  <h1>Chuck Norris Facts</h1>
+  <?php 
 		 if ($connection->connect_error) {
 			die('Connect Error: ' . $connection->connect_error);
 			} else {
@@ -26,20 +23,21 @@ $connection->set_charset("utf8");
 			}
 		 
 		 ?>
-	</header>
-         <?php 
-		 $jokedata = $connection->query("SELECT * FROM joke ORDER BY id DESC"); 
-		 $joke = $data->fetch_assoc();
+</header>
+<?php 
+		 $jokesdata = $connection->query("SELECT * FROM jokes ORDER BY id DESC"); 
+		 $jokes = $jokesdata->fetch_assoc();
 		 ###############################################################################################
 		 # Oh my god - I need a way to render ALL records from the database, not only the last one :-( #
 		 # This makes me sick...                                                                       # 
 		 ###############################################################################################
-		 print_r($joke);
+		 print_r($jokes);
 			echo '<!-- single Chuck Norris joke start -->
 			<div class="joke">
-					<img src="' . $joke['img'] . '" class="norris_pic" alt="Chuck Norris caricature"/>
-					<h2>' . $joke['joke'] .  '</h2>	       
+					<img src="' . $jokes['img'] . '" class="norris_pic" alt="Chuck Norris caricature"/>
+					<h2>' . $jokes['joke'] .  '</h2>	       
             </div>';
 			echo '<!-- single joke end -->';
-		 ?>  
-</body> </html>
+		 ?>
+</body>
+</html>
